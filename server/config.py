5327405aac0782a330d8b66825c81f2264fdb1bc
Tanatron <163878832+TanatronJitjam@users.noreply.github.com>
2026-08-09 02:26:24 +0700

    Add files via upload

---
+++ server/config.py
@@ -0,0 +1,10 @@
+# config.py
+MQTT_BROKER = 'localhost'
+MQTT_PORT = 1883
+DB_CONFIG = {
+    'user': 'YOUR_DB_USER', 
+    'password': 'YOUR_DB_PASSWORD',
+    'host': 'YOUR_DB_HOST',
+    'database': 'YOUR_DB_NAME',
+    'raise_on_warnings': True
+}
